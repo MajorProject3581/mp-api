c04d56bd5185ab5fd3494e183350c39745fe6119
VedantKale08 <104070347+VedantKale08@users.noreply.github.com>
2022-11-09 11:21:10 +0530

    fc

---
+++ data.js
@@ -0,0 +1,63 @@
+const cors = require('cors');
+const express = require('express');
+const router = express.Router();
+const college = require('./dataSchema');
+const mongoose = require('mongoose');
+const { response } = require('express');
+
+router.post("/",(req,res,next)=>{
+    const data = new college({
+    _id:new mongoose.Types.ObjectId,
+    Name:req.body.Name,
+    AllDept:req.body.AllDept,
+    AllCaste:req.body.AllCaste,
+    Dept:req.body.Dept,
+    Location:req.body.Location,
+    Website:req.body.Website,
+    Autonomy_status:req.body.Autonomy_status,
+    College_Status:req.body.College_Status,
+    HostelAvailability:req.body.HostelAvailability,
+    Rating:req.body.Rating,
+    LogoURL:req.body.LogoURL,
+    Pdflink:req.body.Pdflink,
+    minFees:req.body.minFees,
+    maxFees:req.body.maxFees
+    })
+    data.save()
+    .then(result=>{
+        console.log(result);
+        res.status(200).json({
+            college : result
+        })
+    })
+    .catch(err=>{
+        console.log('error');
+        res.status(500).json({
+            error:err
+        })
+    })
+})
+
+router.get("/colleges", async (req,res,next)=>{
+    let College = await college.find();
+    if(College.length>0){
+        res.status(200).json({
+            data:College
+        })
+    }else{
+        res.send({result:"No Results Found"})
+    }
+})
+
+router.get("/colleges/:Name",async (req,res)=>{
+    let results = await college.find({Name:req.params.Name});
+    if(results){
+        res.status(200).json({
+            data:results
+        })
+    }else{
+        res.send({results:"No Record found"})
+    }
+})
+
+module.exports = router;
